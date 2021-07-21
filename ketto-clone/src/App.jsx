9e1d5d16cf9dfdd07618c0750a6833322d13cc42
Sudhir <82999615+devSudhir@users.noreply.github.com>
2021-07-21 11:20:26 +0530

    Support tool container comiited

--- ketto-clone/src/App.jsx
+++ ketto-clone/src/App.jsx
@@ -1,10 +1,14 @@
-import { Data } from "./Components/Trend/Data"
-import { NavBar } from "./Components/NavBar/NavBar"
+//import { Data } from "./Components/Trend/Data"
+//import { NavBar } from "./Components/NavBar/NavBar"
+/* import { ImageGallery } from "./Components/ImageGallery/ImageGallery";
+import { ToolContainer } from "./Components/ToolCategories/ToolContainer"; */
 function App() {
   return (
-    <div >
-      <NavBar />
-      <Data />
+    <div>
+      {/*  <NavBar />
+      <Data /> */}
+      {/* <ImageGallery />
+      <ToolContainer /> */}
     </div>
   );
 }
