3f0bce49e7a44e4afc4b6f560d5236588704c0c3
Ramlala Yadav <easyexam01@gmail.com>
2021-07-22 12:46:36 +0530

    added files

--- ketto-clone/src/App.jsx
+++ ketto-clone/src/App.jsx
@@ -1,17 +1,22 @@
-//import { Data } from "./Components/Trend/Data"
-//import { NavBar } from "./Components/NavBar/NavBar"
-/* import { ImageGallery } from "./Components/ImageGallery/ImageGallery";
-import { ToolContainer } from "./Components/ToolCategories/ToolContainer"; */
-
-//import { SuccessStoriesContainer } from "./Components/SuccessSlider/SuccessStoriesContainer";
+import { Data } from "./Components/Trend/Data"
+import { NavBar } from "./Components/NavBar/NavBar"
+import { ImageGallery } from "./Components/ImageGallery/ImageGallery";
+import { ToolContainer } from "./Components/ToolCategories/ToolContainer";
+import { SectionFirst } from "./Components/SectionFirst/SectionFirst"
+import { FundRaiserSteps } from "./Components/FundRaiserSteps/FundRaiserSteps"
+import { Browse } from "./Components/Browse/Brows"
+import { SuccessStoriesContainer } from "./Components/SuccessSlider/SuccessStoriesContainer"
 function App() {
   return (
     <div>
-      {/*  <NavBar />
-      <Data /> */}
-      {/* <ImageGallery />
-      <ToolContainer /> */}
-      {/* <SuccessStoriesContainer /> */}
+      <NavBar />
+      <SectionFirst />
+      <Data />
+      <FundRaiserSteps />
+      <ImageGallery />
+      <ToolContainer />
+      <Browse />
+      <SuccessStoriesContainer />
     </div>
   );
 }
